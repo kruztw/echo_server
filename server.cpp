30d50c44e37f0ef8613caf614b26f81ca820d81b
kruztw <kruztw@gmail.com>
2022-01-07 17:41:07 +0800

    rewrite server.cpp

--- server.cpp
+++ server.cpp
@@ -1,25 +1,18 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <cstdio>
+#include <iostream>
+#include <cstring>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 #include <pthread.h>
 
-#include <iostream>
-#include <string>
-#include <sstream>
+using std::cout;
 
-using namespace std;
 
-#define BUF_SIZE 1024
-
-char buf[BUF_SIZE+1]; /* message buffer */
+constexpr int BUF_SIZE = 1024;
+constexpr int MAX_CONN = 10000;
 
 void *handle_client(void *arg);
+
 
 void error(const char *msg)
 {
@@ -27,17 +20,19 @@ void error(const char *msg)
   exit(1);
 }
 
-void string_return(int m_sockfd)
+
+// Echos back the messages received from clients
+void echoBack(int sockfd)
 {
   int n;
   char buf[BUF_SIZE] = {};
   
   again:
-    while ((n = recv(m_sockfd, buf, BUF_SIZE, 0)) > 0)
-      send(m_sockfd, buf, n, 0);
+    while ((n = recv(sockfd, buf, BUF_SIZE, 0)) > 0)
+      send(sockfd, buf, n, 0);
 
-    if(n < 0 && errno==EINTR) {  // Interrupt occured
-      if(errno == EINTR) {
+    if (n < 0) {
+      if(errno == EINTR) { // Interrupt occured
         goto again;
       }
       else {
@@ -46,33 +41,26 @@ void string_return(int m_sockfd)
     }
 }
 
+
 void *handle_client(void *arg)
 {
   int child_fd = *(int *)arg;
   delete (int *)arg;
 
+  // marks this thread as unjoinable
   pthread_detach(pthread_self());
-  string_return(child_fd);  //Do the load balancing.
+  echoBack(child_fd);
   close(child_fd);
 
   return NULL;
 }
 
+
 int main(int argc, char **argv)
 {
-  int parentfd;                    /* parent socket */
-  int *childfd;                    /* child socket */
-  int portno;                      /* port txo listen on */
-  int clilen;                      /* byte size of client's address */
-  struct sockaddr_in serveraddr;   /* server's addr */
-  struct sockaddr_in clientaddr;   /* client addr */
-  struct hostent *hostp;           /* client host info */
-  char *hostaddrp;                 /* dotted decimal host addr string */
-  int optval;                      /* flag value for setsockopt */
-  int n;                           /* message byte size */
-  int tid;                         /* thread ID */
+  struct sockaddr_in server_addr, client_addr; 
+  int portno;
 
-  pthread_t thread;
 
   if (argc != 2) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -80,40 +68,41 @@ int main(int argc, char **argv)
   }
 
   portno = atoi(argv[1]);
-  parentfd = socket(AF_INET, SOCK_STREAM, 0);
-
+  int parentfd = socket(AF_INET, SOCK_STREAM, 0);
+  
   if (parentfd < 0)
     error("ERROR opening socket");
 
-  optval = 1;
-  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
-         (const void *)&optval, sizeof(int));
+  int optval = SOL_SOCKET;
+  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
 
-  bzero((char *)&serveraddr, sizeof(serveraddr));
+  memset((char *)&server_addr, 0, sizeof(server_addr));
 
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons((unsigned short)portno);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons((unsigned short)portno);
 
-  if (bind(parentfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) {
+  if (bind(parentfd, (struct sockaddr *) &server_addr, 
+	   sizeof(server_addr)) < 0) {
     error("ERROR on binding");
   }
 
-  if (listen(parentfd, 10000) < 0) /* allow 5 requests to queue up */
+  if (listen(parentfd, MAX_CONN) < 0)
     error("ERROR on listen");
 
 
-  clilen = sizeof(clientaddr);
+  int clilen = sizeof(client_addr);
+  pthread_t thread;
+
   while (true) {
-    childfd = new int;
+    int *childfd = new int; // Use Dynamic allocation to avoid race condition
 
     cout << "Waiting for connections...\n";
-    *childfd = accept(parentfd, (struct sockaddr *)&clientaddr,
+    *childfd = accept(parentfd, (struct sockaddr *)&client_addr,
 		      (socklen_t *)&clilen);
 
     if (*childfd < 0 ) {
-      if(errno!=EINTR) {
+      if (errno != EINTR) {
         error("ERROR on accept");
       } else {
         continue;
@@ -121,7 +110,7 @@ int main(int argc, char **argv)
     }
 
     cout << "Connected!\n";
-    if ((tid = pthread_create(&thread, NULL, &handle_client, childfd)) < 0) {
+    if (int tid = pthread_create(&thread, NULL, &handle_client, childfd); tid < 0) {
       fprintf(stderr, "thread create failed (%d)\n", tid);
       delete childfd;
     }
