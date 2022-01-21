40231329131ddc04d93329415509726541524a34
kruztw <kruztw@gmail.com>
2022-01-21 11:31:15 +0800

    server.cpp: support non-blocking & remove redundant goto

--- server.cpp
+++ server.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <mutex>
 #include <queue>
+#include <ctime>
+#include <fcntl.h>
 
 using std::cout;
 using std::cerr;
@@ -15,14 +17,17 @@ using std::vector;
 using std::mutex;
 using std::queue;
 
-static void *handle_client(void *arg);
+static void *handleClient(void *arg);
 static void init();
 void error(const char *msg);
 
+#define NON_BLOCKING
+
 static constexpr int BUF_SIZE = 1024;
 static constexpr int MAX_CONN = 10000;
+static constexpr int THREAD_STACK_SIZE = 1024 * 1024; // 1M
 static mutex conns_lock;
-static constexpr int MAX_TERM_TIME = 5; // the maximum shutdown duration (second) when server received SIGTERM
+static constexpr int MAX_TERM_TIME = 5; // the maximum shutdown duration (second)
 static bool shutdown_event;
 
 static queue<int> conns_ids;    // free idx in conns
@@ -78,6 +83,8 @@ private:
 };
 
 
+
+
 void error(const char *msg)
 {
   perror(msg);
@@ -91,9 +98,8 @@ static void sigtermHandler(int sigNum)
   shutdown_event = true;
   
   // make sure each connections is disconnected before we leave.
-  for (int i = 0; i < MAX_TERM_TIME; ++i) {
+  for (int i = 0; i < MAX_TERM_TIME*2; ++i) {
     sleep(1);
-
     int j;
     for (j = 0; j < MAX_CONN; ++j)
       if (conns[j])
@@ -102,26 +108,6 @@ static void sigtermHandler(int sigNum)
     if (j == MAX_CONN)
       goto shutdown;
   }
-
-
-  // Force shutdown
-  forece_shutdown:
-    // TODO: It might cause starvation if there are plenty of clients attemp to connect.
-    //       However, the number of concurrent connections is limited by MAX_CONNS.
-    //       So, it is far away to cause starvation.
-    {
-      cerr << "Time's up. Force shutdown ..." << endl;
-      std::unique_lock<mutex> lock(conns_lock, std::defer_lock);
-
-      lock.lock();
-      for (int i = 0; i < MAX_CONN; ++i) {
-        if (conns[i] && pthread_cancel(*conns[i]) < 0)
-            error("pthread_cacnel");
-      }
-      lock.unlock();
-
-      cerr << "shutdown" << endl;
-    }
 
   shutdown:
     exit(1);
@@ -133,56 +119,60 @@ static void echoBack(Conn *conn)
   int n;
   char buf[BUF_SIZE] = {};
   const int sockfd = conn->getChildFD();
+  time_t shutdown_time;
   
-  again:
-    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-    while ((n = recv(sockfd, buf, BUF_SIZE, 0)) > 0) {
-      sendAgain:
-        if (int tmp = send(sockfd, buf, n, 0); tmp < 0) {
-          if (errno == EINTR) {
-            goto sendAgain;
-          } else if (errno == ECONNRESET) {
-            goto disconnect;
-          } else {
-            error("Echo error : ");
-          }
-        }
-    }
-
-    if (shutdown_event && conn->getState() != State::kShutdown) {
-      char msg[0x100];
-      snprintf(msg, sizeof(msg), "Server will shutdown in %d seconds !\n", MAX_TERM_TIME);
-
-      sendAgain2:
-        if (int tmp = send(sockfd, msg, strlen(msg), 0); tmp < 0) {
-          if (errno == EINTR) {
-            goto sendAgain2;
-          } else if (errno == ECONNRESET) {
-            goto disconnect;
-          } else {
-            error("Echo error : ");
-          }
-        }
-        conn->setState(State::kShutdown);
-    }
-
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-
-    if (n < 0) {
-      if(errno == EINTR || errno == EAGAIN) {
-        goto again;
-      } else if (errno == ECONNRESET) { // client disconnect
+  while ((n = recv(sockfd, buf, BUF_SIZE, 0)) != 0) {
+    // recv error
+    if (n < 0 && errno != EAGAIN) { // Exclude EAGAIN so that check event can happen
+      if (errno == EINTR) {
+	continue;
+      }	else if (errno == ECONNRESET) { // client disconnect
         goto disconnect;
       } else {
-        error("Echo error : ");
+        error("Echo error");
       }
     }
-    
+
+    while (n > 0 && send(sockfd, buf, n, 0) < 0) {
+      if (errno == EAGAIN || errno == EINTR) {
+        continue;
+      } else if (errno == ECONNRESET) {
+        goto disconnect;
+      } else {
+        error("Echo error");
+      }
+    }
+
+    // check event
+    if (shutdown_event && conn->getState() != State::kShutdown) {
+      char msg[256];
+      snprintf(msg, sizeof(msg), "Server will shutdown in %d seconds !\n", MAX_TERM_TIME);
+
+      while (send(sockfd, msg, strlen(msg), 0) < 0) {
+          if (errno == EAGAIN || errno == EINTR) {
+            continue;
+          } else if (errno == ECONNRESET) {
+            goto disconnect;
+          } else {
+            error("Echo error");
+          }
+      }
+
+      shutdown_time = time(NULL);
+      conn->setState(State::kShutdown);
+    }
+
+    if (conn->getState() == State::kShutdown &&
+        time(NULL) - shutdown_time >= MAX_TERM_TIME) {
+         goto disconnect;
+    }
+  }
+
   disconnect:
     return;
 }
 
-static void *handle_client(void *arg)
+static void *handleClient(void *arg)
 {
   Conn *conn = static_cast<Conn *>(arg);
   // Avoid thread occupies SIGTERM handler and make sure async signal safe
@@ -232,10 +222,19 @@ int main(int argc, char **argv)
   int optval = SOL_SOCKET;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) < 0)
     error("ERROR setsockopt SO_REUSEADDR");
+  
+  #ifdef NON_BLOCKING
+    // using select in echoBack is another way, but this way is a per-call option
+    struct timeval timeout = {1, 0}; // 1s
+    if(setsockopt(server_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0)
+      error("ERROR setsockopt SO_RCVTIMEO");
+    if(setsockopt(server_fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0)
+      error("ERROR setsockopt SO_RCVTIMEO");
+  
+    int flags = fcntl(server_fd, F_GETFL, 0);
+    fcntl(server_fd, F_SETFL, flags | O_NONBLOCK);
+  #endif
 
-  struct timeval timeout = {1, 0}; // 1s
-  if(setsockopt(server_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0)
-    error("ERROR setsockopt SO_RCVTIMEO");
 
   memset((char *)&server_addr, 0, sizeof(server_addr));
 
@@ -254,15 +253,41 @@ int main(int argc, char **argv)
   int clilen = sizeof(client_addr);
 
   cout << "Waiting for connections...\n";
+
+  pthread_attr_t attr;
+  if (pthread_attr_init(&attr) != 0)
+    error("pthread_attr_init");
+  if (pthread_attr_setstacksize(&attr, THREAD_STACK_SIZE) != 0)
+    error("pthread_attr_setstack_size");
+
   while (true) {
+
+    #ifdef NON_BLOCKING
+      // Do it every time, since readfds will change after select
+      fd_set rdset;
+      FD_ZERO(&rdset);
+      FD_SET(server_fd, &rdset);
+
+      struct timeval timeout = {1, 0}; // 1s
+      int result = select(server_fd+1, &rdset, NULL, NULL, &timeout);
+
+      if (result == -1) {
+        perror("select");
+	goto shutdown;
+      } else if (result == 0) {
+	continue;
+      }
+    #endif
+    
     int child_fd = accept(server_fd, (struct sockaddr *)&client_addr,
 		                      (socklen_t *)&clilen);
 
     if (child_fd < 0 ) {
-      if (errno != EINTR && errno != EAGAIN) {
-        error("ERROR on accept");
-      } else {
+      if (errno == EINTR && errno == EAGAIN) {
         continue;
+      } else {
+        perror("ERROR on accept");
+	goto shutdown;
       }
     }
 
@@ -270,11 +295,17 @@ int main(int argc, char **argv)
     Conn *conn = new Conn(thread, child_fd); // Use Dynamic allocation to avoid race condition
 
     cout << "Connected!\n";
-    if (int tid = pthread_create(thread, NULL, &handle_client, conn); tid < 0) {
+    if (int tid = pthread_create(thread, NULL, &handleClient, conn); tid < 0) {
       cerr << "thread create failed (" << tid << ")\n";
       delete conn;
     }
   }
+
+shutdown:
+  if (pthread_attr_destroy(&attr))
+    error("pthread_attr_destroy");
+
+  close(server_fd);
 
   return 0;
 }
